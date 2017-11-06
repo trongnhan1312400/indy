886b33254f46c90e795cc6c8484dd630f3348b38
Nhan Nguyen - 6187 <nhan.trong.nguyen@logigear.com>
2017-11-06 15:39:54 +0700

    first commit

--- Scenario03_Verify_Version_Displays_and_Test_Connect_Disconnect.py
+++ Scenario03_Verify_Version_Displays_and_Test_Connect_Disconnect.py
@@ -2,25 +2,25 @@ from indy import agent, ledger, pool, signus
 import json
 
 
-def test():
+async def test():
     seed_trustee1 = "000000000000000000000000Steward1"
 
     print("Begin")
 
     print("Connect to test")
-    pool_handle = pool.open_pool_ledger("test", None)
+    pool_handle = await pool.open_pool_ledger("test", None)
     print(pool_handle)
 
     print("new key seed 000000000000000000000000Steward1")
-    (sender_did, sender_verkey, sender_pk) = signus.create_and_store_my_did("default", json.dumps({"seed": seed_trustee1}))
+    (sender_did, sender_verkey, sender_pk) = await signus.create_and_store_my_did("default", json.dumps({"seed": seed_trustee1}))
 
     print("close pool")
-    pool.close_pool_ledger(pool_handle)
+    await pool.close_pool_ledger(pool_handle)
 
     print("reconnect pool")
-    pool_handle = pool.open_pool_ledger("test", None)
+    pool_handle = await pool.open_pool_ledger("test", None)
 
-    pool.close_pool_ledger(pool_handle)
+    await pool.close_pool_ledger(pool_handle)
 
 
 test()
