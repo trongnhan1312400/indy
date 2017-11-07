4f2045176d41281d84079532fca78554ecee1034
trongnhan1312400 <trongnhan1312400@gmail.com>
2017-11-07 18:49:44 +0700

    Update Scenario03_Verify_Version_Displays_and_Test_Connect_Disconnect.py

--- Scenario03_Verify_Version_Displays_and_Test_Connect_Disconnect.py
+++ Scenario03_Verify_Version_Displays_and_Test_Connect_Disconnect.py
@@ -13,21 +13,31 @@ async def test():
     new_wallet_created = "New wallet Default created"
     pool_config_txn = ".sovrin/pool_transactions_sandbox_genesis"
     pool_config = json.dumps({"genesis_txn": str(pool_config_txn)})
+    try:
+        pool.create_pool_ledger_config(pool_name, pool_config)
+    except IndyError as E:
+        print("Try to create new pool")
+        pool.delete_pool_ledger_config(pool_name)
+        pool.create_pool_ledger_config(pool_name, pool_config)
 
     try:
         await wallet.create_wallet(pool_name, wallet_name, None, None, None)
     except IndyError as E:
-        await wallet.delete_wallet(wallet_name, None)
         print("Try to create wallet")
+        await wallet.delete_wallet(wallet_name, None)
         await wallet.create_wallet(pool_name, wallet_name, None, None, None)
 
     wallet_handle = await wallet.open_wallet(wallet_name, None, None)
 
     await signus.create_and_store_my_did(wallet_handle, json.dumps({"seed": seed_trustee01}))
 
+    print("Connect")
     pool_handle = await pool.open_pool_ledger(pool_name, None)
 
+    print("Disconnect")
     await pool.close_pool_ledger(pool_handle)
+    
+    print("Reconnect")
     pool_handle = await pool.open_pool_ledger(pool_name)
 
 
